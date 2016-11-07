324c33494bf64528ef5dce1dd9f783e69268b03f
Karthikeyan Thamilselvan <sridhar.vadivelu@fxpal.com>
2016-11-07 16:01:38 +0530

    JWT Auth changes

--- src/main/java/com/karthik/lib/app/Lib2Application.java
+++ src/main/java/com/karthik/lib/app/Lib2Application.java
@@ -2,11 +2,21 @@ package com.karthik.lib.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.karthik.lib.app")
 public class Lib2Application {
+	@Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/library/*");
+
+        return registrationBean;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lib2Application.class, args);
